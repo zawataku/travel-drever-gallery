1cc7145c1adacf690c7e506f53bfea6d114c18ca
ざわたく <c1254976@st.kanazawa-it.ac.jp>
2025-11-05 13:28:33 +0900

    fix: 環境構築やり直し

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+  plugins: [react()],
+})
